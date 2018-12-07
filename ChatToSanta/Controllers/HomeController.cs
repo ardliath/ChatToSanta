a44c513d98b04b17b54d795ee9812e398abde9df
Adam Griffiths <Adam.Griffiths@callcreditgroup.com>
2018-12-07 15:48:29 +0000

    Changed message

--- ChatToSanta/Controllers/HomeController.cs
+++ ChatToSanta/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace ChatToSanta.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "This is a demo application.";
 
             return View();
         }
