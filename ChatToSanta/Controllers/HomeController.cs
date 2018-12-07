9d08c34fec285a2800589c8516653f8ca9108357
Adam Griffiths <Adam.Griffiths@callcreditgroup.com>
2018-12-07 15:46:40 +0000

    Removed the contact us page - not needed

--- ChatToSanta/Controllers/HomeController.cs
+++ ChatToSanta/Controllers/HomeController.cs
@@ -19,12 +19,5 @@ namespace ChatToSanta.Controllers
 
             return View();
         }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
