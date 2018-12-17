2208bbbc8cf4bd28d11087126285c0825e92215b
LewcasThunder <jtl.harrison@gmail.com>
2018-12-17 15:12:42 +0000

    Update HomeController.cs

--- ChatToSanta/Controllers/HomeController.cs
+++ ChatToSanta/Controllers/HomeController.cs
@@ -24,19 +24,8 @@ namespace ChatToSanta.Controllers
         }
         
         private string DinosaurOfMaliciousIntent()
-        {
-                            __.--'~~~~~`--.
-         ..       __.    .-~               ~-.
-         ((\     /   `}.~                     `.
-          \\\  .{     }               /     \   \
-      (\   \\~~       }              |       }   \
-       \`.-~ -@~     }  ,-,.         |       )    \
-       (___     ) _}  (    :        |    / /      `._
-        `----._-~.     _\ \ |_       \   / /-.__     `._
-               ~~----~~  \ \| ~~--~~~(  + /     ~-._    ~-._
-                         /  /         \  \          ~--.,___~_-_.
-                      __/  /          _\  )
-                    .<___.'         .<___/  
+        {         
+            return "DinosaurOfPeaceAndJoy";
         }
 
         [HttpPost]
